e602aab776fa4e551ae13973dc8484a04a8a9a2a
Sam Stern <samuelostern@gmail.com>
2016-05-10 13:15:24 +0100

    adding rrl

---
+++ pybrain/rl/learners/directsearch/rrl.py
@@ -0,0 +1,83 @@
+from numpy import tanh, sign
+from random import random
+from pybrain.rl.learners.directsearch.directsearch import DirectSearchLearner
+from pybrain.rl.learners.learner import DataSetLearner, ExploringLearner
+
+class RRL(DirectSearchLearner, DataSetLearner, ExploringLearner):
+    """Recurrent Reinforcement Learning from Moody and Saffell (2001)"""
+
+    def __init__(self):
+        numParameters=3 #TODO: don't have this hardcoded
+        self.A=0.0 #moving average returns
+        self.B=0.01 #moving average std
+        self.delta=0.5 #transaction cost
+        self.eta = 0.1 #moving average decay parameter
+        self.mu = 1.0 # invested capital
+        self.rf = 0.0 #risk free rate
+        self.stepSize=0.01
+        self.thetas = self._initializeParameters(numParameters) # weights
+        self.F = [0.0] # last periods output
+        self.dFt_dTheta=[0.0 for i in range(numParameters)]
+        self.paramUpdateThreshold=0.01 #when to stop ascending the gradient
+
+        # create default explorer
+        self._explorer = None
+
+
+    def explore(self, state, action):
+        # forward pass of exploration
+        explorative = ExploringLearner.explore(self, state, action)
+        return explorative
+
+    def learn(self):
+        assert self.dataset != None
+        assert self.module != None
+        self.thetas=self.module._params.tolist()
+        #changing notation to that used in the paper by Moody and Saffell. R_t is the reward
+        sequenceNumber=self.dataset.getNumSequences()-1
+        _state, _lastAction, R_t = self.dataset.getSequence(sequenceNumber)
+        R_t = R_t[-1,0] # want it as a list
+        #self.F.extend(_lastAction[-1].tolist())
+        self.F.append(_lastAction[-1,0])
+        phi = [1.0,_state[-1,0],self.F[-2]] #TODO: Make sure this still works when _state is more than just 1D
+        r_t = _state[-1,0] # TODO: Make sure that the target timeseries return is always the first row
+
+
+
+        gradient=self.calculateGradient(R_t,phi,r_t)
+        for i in range(len(self.thetas)):
+            self.thetas[i]=self.thetas[i]+self.stepSize*gradient[i]
+
+        self.module._setParameters(self.thetas)
+
+        self.A=self.A+self.eta*(R_t-self.A)
+        self.B=self.B+self.eta*((R_t**2)-self.B)
+
+        # TODO: implement parameter update (see policygradient for an example to follow) as well as updating A,B etc.
+
+    def calculateGradient(self,R_t,phi,r_t):
+        """phi is the design matrix phi={1,F[t-1],x1,x2,...}
+        i is the index of the parameter to update
+        R_t is the return of the portfolio at time t
+        r_t is the return of the risky asset at time t
+        """
+        t=len(self.F)-1
+        new_dFt_dTheta=[0.0 for _ in self.thetas] # initialize temporary dFt_dTheta
+        dFt_dTheta=[0.0 for _ in self.thetas]
+        dS_dTheta=[0.0 for _ in self.thetas]
+        #do the parts that are constant across parameters outside of the loop
+        dU_dR=self.eta*(self.B-self.A*R_t)/((self.B-self.A**2)**(3.0/2))
+        dR_dFt=-self.mu*self.delta*sign(self.F[t]-self.F[t-1])
+        dR_dFtMin1=self.mu*(r_t-self.rf+self.delta*(sign(self.F[t]-self.F[t-1])))
+        thetaPhi=sum([x*y for x,y in zip(phi,self.thetas)])
+        #calculating gradient for each parameter
+        for i in range(len(self.thetas)): # for each parameter
+            new_dFt_dTheta[i]=(1-(tanh(thetaPhi)**2))*(phi[i]+self.thetas[i]*self.dFt_dTheta[i])
+            dS_dTheta[i]=dU_dR*((dR_dFt*new_dFt_dTheta[i])+(dR_dFtMin1*self.dFt_dTheta[i]))
+        self.dFt_dTheta=new_dFt_dTheta
+        return dS_dTheta
+
+    def _initializeParameters(self,numParameters):
+        # adding 1 to the number of parameters to include the bias term theta_0
+        return [random() for i in range(numParameters)]
+
